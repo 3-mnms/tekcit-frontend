dd133a01482942bfdd129883b1dca8265f295df6
hyahn0 <gmldbs0725@gmail.com>
2025-07-30 13:33:55 +0900

    setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
+import path from 'path';
+
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@app',
+        replacement: path.resolve(__dirname, 'src/app'),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@home',
+        replacement: path.resolve(__dirname, 'src/pages/home'),
+      },
+      {
+        find: '@shared',
+        replacement: path.resolve(__dirname, 'src/shared'),
+      },
+      {
+        find: '@icon',
+        replacement: path.resolve(__dirname, 'src/shared/assets/icon'),
+      },
+    ],
+  },
 })
