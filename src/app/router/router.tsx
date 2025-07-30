868b957260afa2425e8b61b5628cc04cc61115e6
hyahn0 <gmldbs0725@gmail.com>
2025-07-30 16:19:12 +0900

    setting

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "../../pages/home";
+import HomePage from "@home/index";
 
 export const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
