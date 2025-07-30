f65671e9728f476b50b2cc7f518185b93414c3b4
hyahn0 <gmldbs0725@gmail.com>
2025-07-30 17:27:26 +0900

    MNMS-64 feat: 공통 컴포넌트(버튼) 추가

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "@home/index";
+import HomePage from "@/pages/home/button_test";
 // import HomePage from "../../pages/home/index";
 
 export const router = createBrowserRouter([
