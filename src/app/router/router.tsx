f6f07a85422373cbf7026d6186f270f1249c4700
MinJeong <106390284+Mineong@users.noreply.github.com>
2025-08-01 17:13:35 +0900

    MNMS-120 feat: 일반 결제 컴포넌트 UI 추가

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "@components/payment/CardSimplePayment";
+// import HomePage from "@components/payment/CardSimplePayment";
 // import HomePage from "@components/payment/WalletPayment";
+import HomePage from "@components/payment/GeneralCardPayment";
 // import HomePage from "../../pages/home/index";
 
 export const router = createBrowserRouter([
---
+++ src/components/payment/CardSelectBox.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '@components/payment/CardSelectBox.css';
+
+interface CardSelectBoxProps {
+  selectedCard: string;
+  onSelect: (card: string) => void;
+}
+
+const cardCompanies = [
+  '', '신한', '현대', '비씨', 'KB국민', '삼성', '롯데', '하나', 'NH', '우리'
+];
+
+const CardSelectBox: React.FC<CardSelectBoxProps> = ({ selectedCard, onSelect }) => {
+  return (
+    <select
+      className="card-select-dropdown"
+      value={selectedCard}
+      onChange={(e) => onSelect(e.target.value)}
+    >
+      <option value="">카드를 선택해 주세요</option>
+      {cardCompanies.slice(1).map((card) => (
+        <option key={card} value={card}>
+          {card}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CardSelectBox;
