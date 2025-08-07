653d5d24d8640c7481aeab3050e4922e2c8d1f4a
hhye0 <jhy030123@gmail.com>
2025-08-07 11:14:17 +0900

    MNMS-260 feat: 상품 등록 페이지 공연 날짜 및 시간 컴포넌트 추가

---
+++ src/models/Product.ts
@@ -0,0 +1,42 @@
+export interface FestivalScheduleDTO {
+    dayOfWeek: string;
+    time: string;
+}
+
+export interface ProductType {
+    fname: string; // 페스티벌 이름
+    genrenm: string; // 장르
+    fage: string; // 관람 연령
+    fcltynm: string; // 공연장
+    faddress: string; // 공연장 주소
+    fdto: string; // 공연 시작일
+    fdfrom: string; // 공연 종료일
+    festivalSchedules: FestivalScheduleDTO[]; // 요일, 시간
+    runningTime: string; // 러닝타임
+    availableNOP: string; // 수용인원
+    ticketPrice: string; // 티켓 가격
+    maxPurchase: '제한 없음' | '1장' | '2장' | '3장' | '4장'; // 구매 수량
+    fticketPick: '일괄 배송' | '현장 수령(QR)' | '배송&현장 수령(QR)'; // 티켓 수령 방법
+    story: string; // 상세정보 
+    posterFile: File | null; // 포스터
+    contentFile: File[]; // //상세 파일들
+}
+
+export const initialProductData: ProductType = {
+    fname: '',
+    genrenm: '',
+    fage: '',
+    fcltynm: '',
+    faddress: '',
+    fdto: '',
+    fdfrom: '',
+    festivalSchedules: [],
+    runningTime: '',
+    availableNOP: '', 
+    ticketPrice: '',
+    maxPurchase: '제한 없음',
+    fticketPick: '일괄 배송',
+    story: '',
+    posterFile: null,
+    contentFile: [],
+};
