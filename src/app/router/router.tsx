58f94e22159ac64a839ec15e55ada68feeafadca
hyahn0 <gmldbs0725@gmail.com>
2025-08-12 16:36:35 +0900

    MNMS-331 feat: 사용자 페이지 연결

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,29 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
-import HomePage from "@/pages/home/HomePage";
-import Layout from "@/pages/home/LayoutTestPage";
-import AnnouncementListPage from "@/pages/announcement/AnnouncementListPage";
-import ProductManagePage from "@/pages/productManage/ProductManagePage";
-import ProductRegisterPage from "@/pages/productRegist/ProductRegistPage";
-import OperatManageHostPage from "@/pages/operatManageHost/OperatManageHostPage";
-import OperatManageUserPage from "@/pages/operatManageUser/OperatManageUserPage";
-import ProductDetailPage from "@/pages/productManage/ProductDetailPage";
-import TicketHolderListPage from "@/pages/productManage/TicketHolderListPage";
-import StatisticsPage from "@/pages/productManage/StatisticsPage";
+import { createBrowserRouter } from 'react-router-dom'
+import HomePage from '@/pages/home/HomePage'
+import Layout from '@/pages/home/LayoutTestPage'
+import AnnouncementListPage from '@/pages/announcement/AnnouncementListPage'
+import ProductManagePage from '@/pages/productManage/ProductManagePage'
+import ProductRegisterPage from '@/pages/productRegist/ProductRegistPage'
+import OperatManageHostPage from '@/pages/operatManageHost/OperatManageHostPage'
+import OperatManageUserPage from '@/pages/operatManageUser/OperatManageUserPage'
+import ProductDetailPage from '@/pages/productManage/ProductDetailPage'
+import TicketHolderListPage from '@/pages/productManage/TicketHolderListPage'
+import StatisticsPage from '@/pages/productManage/StatisticsPage'
 
-export const router = createBrowserRouter([
-  { path: "/", element: < ProductRegisterPage/>},
-  { path: "/operatManage/user", element: < OperatManageUserPage/>},
-  { path: "/operatManage/host", element: < OperatManageHostPage/>},
-  { path: "/productRegist", element: < ProductRegisterPage/>},
-  { path: "/announcement", element: < AnnouncementListPage/>},
-  { path: "/productManage", element: < ProductManagePage/>},
-  { path: "/product-detail/:id", element: < ProductDetailPage/>},
-  { path: "/productManage/:id/TicketHolderList", element: < TicketHolderListPage/>},
-  { path: "/productManage/Statistics/:id", element: < StatisticsPage/>},
-  
-  
-  
-  
-  { path: "/button", element: <HomePage /> }, 
-  { path: "/layout", element: <Layout/>},
-]);
+export const router = createBrowserRouter(
+  [
+    { path: '/', element: <ProductRegisterPage /> },
+    { path: '/operatManage/user', element: <OperatManageUserPage /> },
+    { path: '/operatManage/host', element: <OperatManageHostPage /> },
+    { path: '/productRegist', element: <ProductRegisterPage /> },
+    { path: '/announcement', element: <AnnouncementListPage /> },
+    { path: '/productManage', element: <ProductManagePage /> },
+    { path: '/product-detail/:id', element: <ProductDetailPage /> },
+    { path: '/productManage/:id/TicketHolderList', element: <TicketHolderListPage /> },
+    { path: '/productManage/Statistics/:id', element: <StatisticsPage /> },
+
+    { path: '/button', element: <HomePage /> },
+    { path: '/layout', element: <Layout /> },
+  ],
+  { basename: '/admin' },
+)
