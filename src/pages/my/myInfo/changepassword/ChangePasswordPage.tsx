72218df1ad25774f50666ef5afec3263f3623c50
hyahn0 <gmldbs0725@gmail.com>
2025-08-27 16:13:55 +0900

    MNMS-507 feat: 비밀번호 변경 api 연동

--- src/pages/my/myInfo/changepassword/ChangePasswordPage.tsx
+++ src/pages/my/myInfo/changepassword/ChangePasswordPage.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Button from '@/components/common/button/Button';
-import Input from '@/components/common/input/Input'
-import styles from './ChangePasswordPage.module.css';
+import React from 'react'
+import CheckPasswordForm from '@/components/my/myinfo/password/CheckPasswordForm'
+import ResetPasswordForm from '@/components/my/myinfo/password/ResetPasswordForm'
+import styles from './ChangePasswordPage.module.css'
 
 const ChangePasswordPage: React.FC = () => {
+  const [verified, setVerified] = React.useState(false)
+  const [done, setDone] = React.useState(false)
+
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>비밀번호 변경</h2>
-      <div className={styles.card}>
-        <Input type="password" placeholder="현재 비밀번호" />
-        <Input type="password" placeholder="새 비밀번호" />
-        <Input type="password" placeholder="새 비밀번호 확인" />
 
-        <Button className={styles.button}>비밀번호 변경</Button>
-      </div>
+      {!verified && <CheckPasswordForm onVerified={() => setVerified(true)} />}
+
+      {verified && !done && <ResetPasswordForm onSuccess={() => setDone(true)} />}
+
+      {done && <div className={styles.successBox}>✅ 비밀번호가 성공적으로 변경되었어요!</div>}
     </section>
-  );
-};
+  )
+}
 
-export default ChangePasswordPage;
+export default ChangePasswordPage
