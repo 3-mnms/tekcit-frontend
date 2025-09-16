9391bb7e2295e829dd61e4f2644e8ff800038175
Ahn Huiyun <gmldbs0725@gmial.com>
2025-09-16 21:12:37 +0900

    MNMS-689 fix: 주최자 및 관리자 예매 불가하도록 수정

--- src/components/booking/TicketOrderSection.tsx
+++ src/components/booking/TicketOrderSection.tsx
@@ -4,6 +4,8 @@ import { ko } from 'date-fns/locale'
 import 'react-datepicker/dist/react-datepicker.css'
 import Button from '@/components/common/button/Button'
 import styles from './TicketOrderSection.module.css'
+import { useAuthStore } from '@/shared/storage/useAuthStore'
+
 
 type NextPayload = {
   fid?: string
@@ -60,6 +62,9 @@ function makeDemo() {
     maxQty: 4,
   }
 }
+
+const role = useAuthStore((s) => s.user?.role);
+const isUserRole = role === 'USER';
 
 const TicketOrderSection: React.FC<Props> = ({
   fid,
