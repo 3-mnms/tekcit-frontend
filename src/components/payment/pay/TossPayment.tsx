1efab2b15360191786437e44c14e0bef066a373d
rookies-dogun <rookies.dogun@gmail.com>
2025-09-18 11:06:23 +0900

    MNMS-621 fix: portone 디버깅

--- src/components/payment/pay/TossPayment.tsx
+++ src/components/payment/pay/TossPayment.tsx
@@ -111,8 +111,12 @@ const TossPayment = forwardRef<TossPaymentHandle, TossPaymentProps>(
             payMethod: PayMethod.CARD,
             redirectUrl: finalRedirect,
           })
+          console.log("portOneResult : ", portOneResult);
           if (portOneResult?.code === 'success') {
+            
+            console.log("args complete 준비 : ");
             if(args.complete){
+              console.log("args complete 실행 : ");
               args.complete({
                 paymentId: paymentId,
                 status:   'success' ,
